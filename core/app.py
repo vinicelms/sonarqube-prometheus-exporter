4c070d9e0cdf2e9df4826384ec5b711cf59b1faf
Vinicius Celms De Augustinis <vinicius.celms@gmail.com>
2019-10-28 18:27:57 -0300

    Refactor project to use prometheus_client

--- core/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-from flask_restful import Api
-from resources.metrics import Metrics
-
-api_bp = Blueprint('api', __name__)
-api = Api(api_bp)
-
-# Routes
-api.add_resource(Metrics, '/metrics')
---
+++ core/prometheus_exporter.py
@@ -0,0 +1,47 @@
+from prometheus_client.core import GaugeMetricFamily
+import prometheus_client as prom
+import time
+from sonarqube_exporter import get_all_projects_with_metrics
+
+class CustomSonarExporter:
+
+    def __init__(self):
+        pass
+
+    def collect(self):
+        projects = get_all_projects_with_metrics()
+
+        label_list = ['id', 'key']
+        label_values = []
+        value_to_set = None
+
+        for project in projects:
+            label_values.append(project.id)
+            label_values.append(project.key)
+            for metric in project.metrics:
+                for metric_value in metric.values:
+                    if metric_value[0] == 'value':
+                        value_to_set = metric_value[1]
+                    else:
+                        label_list.append(metric_value[0])
+                        label_values.append(metric_value[1])
+
+                gauge = GaugeMetricFamily(
+                    name="sonar_{}".format(metric.key),
+                    documentation=metric.description,
+                    labels=label_list
+                )
+
+                gauge.add_metric(
+                    labels=label_values,
+                    value=value_to_set
+                )
+                yield gauge
+
+if __name__ == "__main__":
+    custom_exporter = CustomSonarExporter()
+    prom.REGISTRY.register(custom_exporter)
+    prom.start_http_server(9120)
+
+    while True:
+        time.sleep(2)
